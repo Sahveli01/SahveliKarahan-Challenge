653ea1cc21236de3c495472e75b0950a9a428da3
Sahveli01 <sahveli94@gmail.com>
2025-10-24 12:54:51 +0300

    change_price.ts hata düzeltme

--- ui/src/utility/admin/change_price.ts
+++ ui/src/utility/admin/change_price.ts
@@ -3,24 +3,14 @@ import { Transaction } from "@mysten/sui/transactions";
 export const changePrice = (packageId: string, listHeroId: string, newPriceInSui: string, adminCapId: string) => {
   const tx = new Transaction();
 
-  // 1. "0.04" gibi bir ondalıklı string'i önce Number'a çevirip MIST ile çarpıyoruz
-  //    (Boşlukları olmayan 1000000000 kullanıyoruz)
-  const priceAsNumber = Number(newPriceInSui) * 1000000000;
+  const newPriceInSui =BigInt(newPriceInSui) * BigInt(1_000_000_000);
 
-  // 2. Bu büyük sayıyı, hata olmasın diye BigInt'e çeviriyoruz
-  const newPriceInMist = BigInt(priceAsNumber);
-
-  // 3. 'target:' formatı yerine yeni formatı kullanıyoruz
   tx.moveCall({
-    package: packageId,
-    module: 'marketplace',
-    function: 'change_the_price',
+    target: `${packageId}::marketplace::change_the_price`,
     arguments: [
       tx.object(adminCapId),
       tx.object(listHeroId),
-      // 4. tx.pure.u64 fonksiyonuna BigInt'i metin (string) olarak veriyoruz.
-      //    Bu, en güvenli yöntemdir.
-      tx.pure.u64(newPriceInMist.toString())
+      tx.pure.u64(newPriceInSui)
     ],
   });
 
