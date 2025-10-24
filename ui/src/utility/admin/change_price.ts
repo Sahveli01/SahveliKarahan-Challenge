9de042f73c67f5bcc3bbba06f822b73c448dbddb
Sahveli01 <sahveli94@gmail.com>
2025-10-24 12:57:38 +0300

    change_price.ts dosyası hata düzeltme

--- ui/src/utility/admin/change_price.ts
+++ ui/src/utility/admin/change_price.ts
@@ -3,14 +3,14 @@ import { Transaction } from "@mysten/sui/transactions";
 export const changePrice = (packageId: string, listHeroId: string, newPriceInSui: string, adminCapId: string) => {
   const tx = new Transaction();
 
-  const newPriceInSui =BigInt(newPriceInSui) * BigInt(1_000_000_000);
+  const newPriceInMist =BigInt(newPriceInSui) * BigInt(1_000_000_000);
 
   tx.moveCall({
     target: `${packageId}::marketplace::change_the_price`,
     arguments: [
       tx.object(adminCapId),
       tx.object(listHeroId),
-      tx.pure.u64(newPriceInSui)
+      tx.pure.u64(newPriceInMist)
     ],
   });
 
